b1eb50ed80d28c3a34f431c9218f795b001a920c
Verta-master <vertyagina@gmail.com>
2017-07-06 21:43:55 +0600

    Update 2017-07-06T15:43:26.644Z

--- js/script.js
+++ js/script.js
@@ -14,15 +14,18 @@ navToggle.addEventListener("click", function() {
 var s1 = document.querySelector(".certify__img--s1");
 var popup = document.querySelector(".modal--s1");
 var close = popup.querySelector(".modal__close--s1");
+var overlay = document.querySelector(".modal-overlay");
 
 s1.addEventListener("click", function(event) {
   event.preventDefault();
+  overlay.classList.add("modal-overlay-show");
   popup.classList.add("modal__show");
 });
 
 close.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.remove("modal__show");
+  overlay.classList.remove("modal-overlay-show");
 });
 
 window.addEventListener("keydown", function(event) {
@@ -39,12 +42,14 @@ var close2 = popup2.querySelector(".modal__close--s2");
 
 s2.addEventListener("click", function(event) {
   event.preventDefault();
+  overlay.classList.add("modal-overlay-show");
   popup2.classList.add("modal__show");
 });
 
 close2.addEventListener("click", function(event) {
   event.preventDefault();
   popup2.classList.remove("modal__show");
+  overlay.classList.remove("modal-overlay-show");
 });
 
 var s3 = document.querySelector(".certify__img--s3");
@@ -53,12 +58,14 @@ var close3 = popup3.querySelector(".modal__close--s3");
 
 s3.addEventListener("click", function(event) {
   event.preventDefault();
+  overlay.classList.add("modal-overlay-show");
   popup3.classList.add("modal__show");
 });
 
 close3.addEventListener("click", function(event) {
   event.preventDefault();
   popup3.classList.remove("modal__show");
+  overlay.classList.remove("modal-overlay-show");
 });
 
 var s4 = document.querySelector(".certify__img--s4");
@@ -67,12 +74,14 @@ var close4 = popup4.querySelector(".modal__close--s4");
 
 s4.addEventListener("click", function(event) {
   event.preventDefault();
+  overlay.classList.add("modal-overlay-show");
   popup4.classList.add("modal__show");
 });
 
 close4.addEventListener("click", function(event) {
   event.preventDefault();
   popup4.classList.remove("modal__show");
+  overlay.classList.remove("modal-overlay-show");
 });
 
 var s5 = document.querySelector(".certify__img--s5");
@@ -81,12 +90,14 @@ var close5 = popup5.querySelector(".modal__close--s5");
 
 s5.addEventListener("click", function(event) {
   event.preventDefault();
+  overlay.classList.add("modal-overlay-show");
   popup5.classList.add("modal__show");
 });
 
 close5.addEventListener("click", function(event) {
   event.preventDefault();
   popup5.classList.remove("modal__show");
+  overlay.classList.remove("modal-overlay-show");
 });
 
 var tour = document.querySelector(".promo__link");
@@ -95,12 +106,14 @@ var close6 = popup6.querySelector(".modal__close--tour");
 
 tour.addEventListener("click", function(event) {
   event.preventDefault();
+  overlay.classList.add("modal-overlay-show");
   popup6.classList.add("modal__show");
 });
 
 close6.addEventListener("click", function(event) {
   event.preventDefault();
   popup6.classList.remove("modal__show");
+  overlay.classList.remove("modal-overlay-show");
 });
 
 var video = document.querySelector(".benefits__btn--up");
@@ -109,12 +122,14 @@ var close7 = popup7.querySelector(".modal__close--video");
 
 video.addEventListener("click", function(event) {
   event.preventDefault();
+  overlay.classList.add("modal-overlay-show");
   popup7.classList.add("modal__show");
 });
 
 close7.addEventListener("click", function(event) {
   event.preventDefault();
   popup7.classList.remove("modal__show");
+  overlay.classList.remove("modal-overlay-show");
 });
 
 var consult = document.querySelector(".btn--consult");
@@ -123,12 +138,14 @@ var close8 = popup8.querySelector(".modal__close--consult");
 
 consult.addEventListener("click", function(event) {
   event.preventDefault();
+  overlay.classList.add("modal-overlay-show");
   popup8.classList.add("modal__show");
 });
 
 close8.addEventListener("click", function(event) {
   event.preventDefault();
   popup8.classList.remove("modal__show");
+  overlay.classList.remove("modal-overlay-show");
 });
 
 var book = document.querySelector(".btn--book");
@@ -137,12 +154,14 @@ var close9 = popup9.querySelector(".modal__close--book");
 
 book.addEventListener("click", function(event) {
   event.preventDefault();
+  overlay.classList.add("modal-overlay-show");
   popup9.classList.add("modal__show");
 });
 
 close9.addEventListener("click", function(event) {
   event.preventDefault();
   popup9.classList.remove("modal__show");
+  overlay.classList.remove("modal-overlay-show");
 });
 
 var price = document.querySelector(".price__link");
@@ -151,12 +170,14 @@ var close10 = popup10.querySelector(".modal__close--price");
 
 price.addEventListener("click", function(event) {
   event.preventDefault();
+  overlay.classList.add("modal-overlay-show");
   popup10.classList.add("modal__show");
 });
 
 close10.addEventListener("click", function(event) {
   event.preventDefault();
   popup10.classList.remove("modal__show");
+  overlay.classList.remove("modal-overlay-show");
 });
 
 var price2 = document.querySelector(".price__link--2");
@@ -165,12 +186,14 @@ var close11 = popup11.querySelector(".modal__close--price2");
 
 price2.addEventListener("click", function(event) {
   event.preventDefault();
+  overlay.classList.add("modal-overlay-show");
   popup11.classList.add("modal__show");
 });
 
 close11.addEventListener("click", function(event) {
   event.preventDefault();
   popup11.classList.remove("modal__show");
+  overlay.classList.remove("modal-overlay-show");
 });
 
 var room = document.querySelector(".rooms__btn");
@@ -179,12 +202,14 @@ var close12 = popup12.querySelector(".modal__close--room1");
 
 room.addEventListener("click", function(event) {
   event.preventDefault();
+  overlay.classList.add("modal-overlay-show");
   popup12.classList.add("modal__show");
 });
 
 close12.addEventListener("click", function(event) {
   event.preventDefault();
   popup12.classList.remove("modal__show");
+  overlay.classList.remove("modal-overlay-show");
 });
 
 var room2 = document.querySelector(".rooms__btn--2");
@@ -193,12 +218,14 @@ var close13 = popup13.querySelector(".modal__close--room2");
 
 room2.addEventListener("click", function(event) {
   event.preventDefault();
+  overlay.classList.add("modal-overlay-show");
   popup13.classList.add("modal__show");
 });
 
 close13.addEventListener("click", function(event) {
   event.preventDefault();
   popup13.classList.remove("modal__show");
+  overlay.classList.remove("modal-overlay-show");
 });
 
 var book2 = document.querySelector(".benefits__btn--book2");
@@ -207,12 +234,14 @@ var close14 = popup14.querySelector(".modal__close--book2");
 
 book2.addEventListener("click", function(event) {
   event.preventDefault();
+  overlay.classList.add("modal-overlay-show");
   popup14.classList.add("modal__show");
 });
 
 close14.addEventListener("click", function(event) {
   event.preventDefault();
   popup14.classList.remove("modal__show");
+  overlay.classList.remove("modal-overlay-show");
 });
 
 var book3 = document.querySelector(".response__link");
@@ -221,12 +250,14 @@ var close15 = popup15.querySelector(".modal__close--book3");
 
 book3.addEventListener("click", function(event) {
   event.preventDefault();
+  overlay.classList.add("modal-overlay-show");
   popup15.classList.add("modal__show");
 });
 
 close15.addEventListener("click", function(event) {
   event.preventDefault();
   popup15.classList.remove("modal__show");
+  overlay.classList.remove("modal-overlay-show");
 });
 
 var phone = document.querySelector(".footer-main__link");
@@ -235,12 +266,13 @@ var close16 = popup16.querySelector(".modal__close--phone");
 
 phone.addEventListener("click", function(event) {
   event.preventDefault();
+  overlay.classList.add("modal-overlay-show");
   popup16.classList.add("modal__show");
 });
 
 close16.addEventListener("click", function(event) {
   event.preventDefault();
   popup16.classList.remove("modal__show");
+  overlay.classList.remove("modal-overlay-show");
 });
-
 
